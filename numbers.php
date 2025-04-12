1b023d0095a07f1a6b45bbef173255552c61a6f8
Christian UWAYEZU <125951952+Alphapanter@users.noreply.github.com>
2025-04-12 19:19:16 +0200

    Add files via upload

---
+++ numbers.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 10;
+$b = 3;
+
+
+echo "Addition: " . ($a + $b) . "<br>";
+echo "Subtraction: " . ($a - $b) . "<br>";
+echo "Multiplication: " . ($a * $b) . "<br>";
+echo "Division: " . ($a / $b) . "<br>";
+echo "Modulus: " . ($a % $b) . "<br>";
+
+
+echo "Power (10^3): " . pow($a, $b) . "<br>";
+
+
+echo "Square root of 10: " . sqrt($a) . "<br>";
+
+
+echo "Rounded division result: " . round($a / $b, 2) . "<br>";
+
+
+echo "Random number between 1 and 100: " . rand(1, 100) . "<br>";
+?>
+
